7c78a0a7b2382118b65ed47cab6863b6e3e23310
tunganh252 <tunganh2521999@gmail.com>
2020-09-03 00:13:25 +0700

    [Implement UI Main]

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet reference tới UI Element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceThree");
+        diceImageView2.image = #imageLiteral(resourceName: "DiceSix");
+//        diceImageView1.alpha = 0.5;
     }
 
-
+    /*Reference tới Button Roll (chú ý action, type và animation)*/
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button Roll Pressed")
+        print("Call \(110+3) now !!!") /*String Interpolation: Chuỗi nội suy */
+         
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour");
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour");
+    }
+    
 }
 
